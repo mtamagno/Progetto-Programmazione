53a9bd2f0a4970a6c03cea58da7840846d185baf
mtamagno <thegreatherochrono@hotmail.it>
2015-01-22 20:07:22 +0100

    Boh

--- librerie/Progetto.h
+++ librerie/Progetto.h
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
 
 /********************STRUTTURA relBin**************************/
 /***** Creo una struttura dove salvare le coppie appartenenti alla Relazione*******/
