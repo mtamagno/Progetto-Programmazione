b4036fd99fdb99fdde7390b38237bf2cdc7a0d73
mtamagno <thegreatherochrono@hotmail.it>
2015-01-22 18:39:53 +0100

    test_terminati

--- Test.c
+++ Test.c
@@ -5,8 +5,9 @@ int main(void){
 	struct	relBin RelazioneBinaria;
 	int scelta;
 	int scan;
+	int test_terminati;
 	scan = 0;
-	
+	test_terminati = 0;
 	printf("\n Programma per effettuare i Test sulla libreria\n");
 	
 	
@@ -21,12 +22,14 @@ int main(void){
 	}
 	if(scelta == 1)
 	RelazioneBinaria = acquisizione(RelazioneBinaria);
+	
 	if(scelta == 2){
 	printf("\n\n .....Test terminati......\n\n");
-	return(0);
+	test_terminati = 1;
 	}
+	
 	scelta = -1;
-	while(scelta != 7){
+	while(scelta != 7 && test_terminati != 1){
 	printf("\n\n Digita il numero corrispondente all'azione che si vuole svolgere\n");
 	printf("\n 1) Test Acquisizione\n 2) Test Stampa\n 3) Test verifica ordine parziale\n 4) Test verifica ordine totale");
 	printf("\n 5) Test verifica relazione d'equivalenza\n 6) Test funzione\n 7) Esci\n" );
@@ -52,7 +55,7 @@ int main(void){
 		check_funzione(RelazioneBinaria);
 	if(scelta == 7){
 		printf("\n\n .....Test terminati......\n\n");
-		return(0);
+		test_terminati = 1;
 		}
 	}
 return(0);
